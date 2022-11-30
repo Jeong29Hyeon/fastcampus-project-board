f0b71741b1c30e9f612a7de21f86f7515139028e
jeong29hyeon <sian0369@naver.com>
2022-11-30 19:31:19 +0900

    #18 - 유저 리포지토리에 애너테이션 추가 및 테스트
    `@Repository` 애너테이션 추가
    Test에서 주입 받기 위해서 사용
    UserAccount 도메인에 userId 부분에 unique 옵션을 넣었기 때문에 다시 테스트 해보았음.

--- fastcampus-project-board/src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
+++ fastcampus-project-board/src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
@@ -50,7 +50,7 @@ class JpaRepositoryTest {
     void givenTestData_whenInserting_thenWorksFine(){
         //given
         long previousCount = articleRepository.count();
-        UserAccount userAccount = userAccountRepository.save(UserAccount.of("sian","pw",null,null,null));
+        UserAccount userAccount = userAccountRepository.save(UserAccount.of("sian1","pw",null,null,null));
         Article article = Article.of(userAccount,"new article","new content","#spring");
 
         //when
